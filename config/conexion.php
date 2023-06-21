3a5182c5a69ba334fb600712874f2594301272f9
Steven-3005 <lsmp3005@gmail.com>
2023-06-21 04:45:56 -0500

    reestructura

---
+++ config/conexion.php
@@ -0,0 +1,16 @@
+<?php
+class Conectar{
+    protected $dbh;
+    protected function Conexion(){
+        try{
+            $conectar = $this->dbh=new PDO("sqlsrv:Server=localhost;Database=CompraVenta","sa","123456");
+            return $conectar;
+        }catch(Exception $e){
+            print "Error Conexion BD". $e->getMessage() ."<br/>";
+            die();
+        }
+    }
+
+}
+
+?>
---
+++ models/Empresa.php
@@ -0,0 +1,51 @@
+<?php
+    class Empresa extends Conectar{
+       /* TODO: Listar registros */
+       public function get_empresa(){
+        $conectar=parent::Conexion();
+        $sql="SP_L_EMPRESA_01";
+        $query=$conectar->prepare($sql);
+        $query->execute();
+        return $query->fetchALL(PDO::FETCH_ASSOC);
+       }
+       /* TODO: Listar registros por ID en especifico */
+       public function get_empresa_x_emp_id($emp_id){
+        $conectar=parent::Conexion();
+        $sql="SP_L_EMPRESA_02 ?";
+        $query=$conectar->prepare($sql);
+        $query->bindValue(1,$emp_id);
+        $query->execute();
+        return $query->fetchALL(PDO::FETCH_ASSOC);
+       } 
+       /* TODO: eliminar o cambiar estado a eliminado */
+       public function delete_empresa($emp_id){
+        $conectar=parent::Conexion();
+        $sql="SP_D_EMPRESA_01 ?";
+        $query=$conectar->prepare($sql);
+        $query->bindValue(1,$emp_id);
+        $query->execute();
+        
+       }
+       /* TODO: Registro de datos */
+       public function insert_empresa($com_id,$emp_nom,$emp_ruc){
+        $conectar=parent::Conexion();
+        $sql="SP_I_EMPRESA_01 ?,?,?";
+        $query=$conectar->prepare($sql);
+        $query->bindValue(1,$com_id);
+        $query->bindValue(2,$emp_nom);
+        $query->bindValue(3,$emp_ruc);
+        $query->execute();
+       }
+       /* TODO: Actualizar Datos */
+       public function update_empresa($emp_id,$com_id,$emp_nom,$emp_ruc){
+        $conectar=parent::Conexion();
+        $sql="SP_U_EMPRESA_01 ?,?,?,?";
+        $query=$conectar->prepare($sql);
+        $query->bindValue(1,$emp_id);
+        $query->bindValue(2,$com_id);
+        $query->bindValue(3,$emp_nom);
+        $query->bindValue(4,$emp_ruc);
+        $query->execute();
+       }      
+    }
+?>
