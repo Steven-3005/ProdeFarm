87730aa4488878de02a9d287107f4b1f22d38a28
jasserG-hack <ramon_18sept@hotmail.com>
2023-07-07 06:57:33 -0500

    arreglos base datos

--- models/Cliente.php
+++ models/Cliente.php
@@ -41,7 +41,7 @@
         $query->execute();
        }
        /* TODO: Actualizar Datos */
-       public function update_cliente($cli_id,$emp_id,$cli_nom,$cli_ruc,$cli_telf,$cli_dirrec,$cli_correo){
+       public function update_cliente($cli_id,$emp_id,$cli_nom,$cli_ruc,$cli_telf,$cli_direcc,$cli_correo){
         $conectar=parent::Conexion();
         $sql="SP_U_CLIENTE_01 ?,?,?,?,?,?,?";
         $query=$conectar->prepare($sql);
@@ -50,7 +50,7 @@
         $query->bindValue(3,$emp_id);
         $query->bindValue(4,$cli_ruc);
         $query->bindValue(5,$cli_telf);
-        $query->bindValue(6,$cli_dirrec);
+        $query->bindValue(6,$cli_direcc);
         $query->bindValue(7,$cli_correo);
         $query->execute();
        }      
